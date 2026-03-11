69d5bcc01fe756d350f713f3b7ddc4222dda3e8a
EMMANUELNZYOKI <emmanuelnzyoki777@gmail.com>
2026-03-11 23:01:14 +0300

    changes done

--- fms.sql
+++ fms.sql
@@ -1,28 +1,60 @@
-CREATE DATABASE fms;
-USE fms;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2026 at 02:23 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admintb`
+--
+
 CREATE TABLE `admintb` (
   `username` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admintb`
+--
 
 INSERT INTO `admintb` (`username`, `password`) VALUES
 ('admin', 'admin123');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
 
 CREATE TABLE `contact` (
   `name` varchar(30) NOT NULL,
   `email` text NOT NULL,
   `contact` varchar(10) NOT NULL,
   `message` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `contact`
+--
 
 INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 ('Anu', 'anu@gmail.com', '7896677554', 'Hey Admin'),
@@ -35,27 +67,125 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 ('Asiq', 'asiq@gmail.com', '9087897564', 'Love your service. Thank you!'),
 ('Jane', 'jane@gmail.com', '7869869757', 'I love your service!');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emptb`
+--
 
 CREATE TABLE `emptb` (
-  `eid` integer primary key AUTO_INCREMENT,
+  `eid` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `salary` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `emptb`
+--
 
-INSERT INTO `emptb` (`username`, `password`, `email`, `phone`, `salary`) VALUES
-('ashok', 'ashok123', 'ashok@gmail.com', '9900000001', 500),
-('arun', 'arun123', 'arun@gmail.com', '9900000002', 600),
-('Dinesh', 'dinesh123', 'dinesh@gmail.com', '9900000003', 700),
-('Ganesh', 'ganesh123', 'ganesh@gmail.com', '9900000004', 550),
-('Kumar', 'kumar123', 'kumar@gmail.com', '9900000005', 800),
-('Amit', 'amit123', 'amit@gmail.com', '9900000006', 1000),
-('Abbis', 'abbis123', 'abbis@gmail.com', '9900000007', 1500),
-('Tiwary', 'tiwary123', 'tiwary@gmail.com', '9900000008', 450);
+INSERT INTO `emptb` (`eid`, `username`, `password`, `email`, `phone`, `salary`) VALUES
+(9, 'kajaxmanuh', 'kajax123', 'kajax@gmail.com', '0719479011', 10000),
+(10, 'irene', 'irene123', 'irene@gmail.com', '0711893999', 60000);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manages`
+--
+
+CREATE TABLE `manages` (
+  `eid` int(11) NOT NULL,
+  `ename` varchar(20) NOT NULL,
+  `plant_id` varchar(20) NOT NULL,
+  `plant_name` varchar(20) NOT NULL,
+  `med_id` varchar(20) NOT NULL,
+  `med_name` varchar(20) NOT NULL,
+  `salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicines`
+--
+
+CREATE TABLE `medicines` (
+  `plant_id` varchar(10) NOT NULL,
+  `med_id` varchar(10) NOT NULL,
+  `med_name` varchar(20) NOT NULL,
+  `med_type` varchar(20) DEFAULT NULL,
+  `med_cost` varchar(20) DEFAULT NULL,
+  `med_desc` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `medicines`
+--
+
+INSERT INTO `medicines` (`plant_id`, `med_id`, `med_name`, `med_type`, `med_cost`, `med_desc`) VALUES
+('P001', 'M001', 'Tafaban', 'Insecticide', '450rs/l', 'Chloropyriphos 20% EC'),
+('P001', 'M002', 'Fame', 'Insecticide', '600rs/30ml', 'Fluendiamide 39.35% SC'),
+('P001', 'M003', 'Sectin', 'Fungicide', '450rs/100g', 'Fenamidone 10% + mancozeb 50% WG'),
+('P002', 'M004', 'Atrataf', 'Herbicide', '500rs/kg', 'Atrazine 50% Wp'),
+('P002', 'M005', 'Tata Metri', 'Herbicide', '1000rs/kg', 'Metribuzin 70% Wp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `method`
+--
+
+CREATE TABLE `method` (
+  `method_id` varchar(10) NOT NULL,
+  `method_name` varchar(20) NOT NULL,
+  `method_type` text DEFAULT NULL,
+  `method_desc` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `method`
+--
+
+INSERT INTO `method` (`method_id`, `method_name`, `method_type`, `method_desc`) VALUES
+('Md001', 'Traditional Method', 'Check basin method', 'Used in level fields'),
+('Md002', 'Traditional Method', 'Furrow Irrigation method', 'Cheap labour'),
+('Md003', 'Traditional Method', 'Strip Irrigation method', 'Used in raise platform'),
+('Md004', 'Modern Method', 'Sprinkler system', 'Used in uneven lands'),
+('Md005', 'Modern Method', 'Drip irrigation method', 'Used when there is scarcity of water');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plant`
+--
+
+CREATE TABLE `plant` (
+  `plant_id` varchar(10) NOT NULL,
+  `plant_name` varchar(20) NOT NULL,
+  `plant_type` varchar(20) DEFAULT NULL,
+  `plant_desc` text DEFAULT NULL,
+  `soil_type` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `plant`
+--
+
+INSERT INTO `plant` (`plant_id`, `plant_name`, `plant_type`, `plant_desc`, `soil_type`) VALUES
+('P001', 'Tomato', 'Food crop', 'Requires 10 months to grow', 'Red loam soil'),
+('P002', 'Sugarcane', 'Food crop', 'Requires 1 year to grow', 'Black soil'),
+('P003', 'Paddy', 'Feed and Food crop', 'Requires 4 months to grow', 'Alluvium soil'),
+('P004', 'Sunflower', 'Oil crop', 'Requires 3 months to grow', 'Sandy loam soil'),
+('P005', 'Cotton', 'Fiber crop', 'Requires 5 months to grow', 'Black soil');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userreg`
+--
 
 CREATE TABLE `userreg` (
   `pid` int(11) NOT NULL,
@@ -66,93 +196,78 @@ CREATE TABLE `userreg` (
   `contact` varchar(10) NOT NULL,
   `password` varchar(30) NOT NULL,
   `cpassword` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `userreg`
+--
 
 INSERT INTO `userreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`) VALUES
-(1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123'),
-(2, 'Alia', 'Bhatt', 'Female', 'alia@gmail.com', '8976897689', 'alia123', 'alia123'),
-(3, 'Shahrukh', 'khan', 'Male', 'shahrukh@gmail.com', '8976898463', 'shahrukh123', 'shahrukh123'),
-(4, 'Kishan', 'Lal', 'Male', 'kishansmart0@gmail.com', '8838489464', 'kishan123', 'kishan123'),
-(5, 'Gautam', 'Shankararam', 'Male', 'gautam@gmail.com', '9070897653', 'gautam123', 'gautam123'),
-(6, 'Sushant', 'Singh', 'Male', 'sushant@gmail.com', '9059986865', 'sushant123', 'sushant123'),
-(7, 'Nancy', 'Deborah', 'Female', 'nancy@gmail.com', '9128972454', 'nancy123', 'nancy123'),
-(8, 'Kenny', 'Sebastian', 'Male', 'kenny@gmail.com', '9809879868', 'kenny123', 'kenny123'),
-(9, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'william123', 'william123'),
-(10, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123'),
-(11, 'Shraddha', 'Kapoor', 'Female', 'shraddha@gmail.com', '9768946252', 'shraddha123', 'shraddha123');
+(12, 'kajax', 'manuh', 'Male', 'kajax@gmail.com', '0719479011', 'kajax123', 'kajax123'),
+(13, 'irene', 'nzisa', 'Female', 'irene@gmail.com', '0711893999', 'irene123', 'irene123');
 
+--
+-- Indexes for dumped tables
+--
 
-CREATE TABLE plant(
-    plant_id VARCHAR(10) PRIMARY KEY,
-    plant_name VARCHAR(20) NOT NULL,
-    plant_type VARCHAR(20),
-    plant_desc TEXT,
-    soil_type VARCHAR(20)
-);
+--
+-- Indexes for table `emptb`
+--
+ALTER TABLE `emptb`
+  ADD PRIMARY KEY (`eid`);
 
-CREATE TABLE medicines(
-    plant_id VARCHAR(10) NOT NULL,
-    med_id VARCHAR(10) PRIMARY KEY,
-    med_name VARCHAR(20) NOT NULL,
-    med_type VARCHAR(20),
-    med_cost VARCHAR(20),
-    med_desc TEXT,
-    FOREIGN KEY (plant_id) REFERENCES plant(plant_id) ON DELETE CASCADE
-);
+--
+-- Indexes for table `medicines`
+--
+ALTER TABLE `medicines`
+  ADD PRIMARY KEY (`med_id`),
+  ADD KEY `plant_id` (`plant_id`);
 
-CREATE TABLE method(
-    method_id VARCHAR(10) PRIMARY KEY,
-    method_name VARCHAR(20) NOT NULL,
-    method_type TEXT,
-    method_desc TEXT
-);
+--
+-- Indexes for table `method`
+--
+ALTER TABLE `method`
+  ADD PRIMARY KEY (`method_id`);
 
-INSERT INTO plant VALUES
-("P001","Tomato","Food crop","Requires 10 months to grow","Red loam soil"),
-("P002","Sugarcane","Food crop","Requires 1 year to grow","Black soil"),
-("P003","Paddy","Feed and Food crop","Requires 4 months to grow","Alluvium soil"),
-("P004","Sunflower","Oil crop","Requires 3 months to grow","Sandy loam soil"),
-("P005","Cotton","Fiber crop","Requires 5 months to grow","Black soil");
+--
+-- Indexes for table `plant`
+--
+ALTER TABLE `plant`
+  ADD PRIMARY KEY (`plant_id`);
 
-INSERT INTO medicines VALUES
-("P001","M001","Tafaban","Insecticide","450rs/l","Chloropyriphos 20% EC"),
-("P001","M002","Fame","Insecticide","600rs/30ml","Fluendiamide 39.35% SC"),
-("P001","M003","Sectin","Fungicide","450rs/100g","Fenamidone 10% + mancozeb 50% WG"),
-("P002","M004","Atrataf","Herbicide","500rs/kg","Atrazine 50% Wp"),
-("P002","M005","Tata Metri","Herbicide","1000rs/kg","Metribuzin 70% Wp");
-
-INSERT INTO method VALUES
-("Md001","Traditional Method","Check basin method","Used in level fields"),
-("Md002","Traditional Method","Furrow Irrigation method","Cheap labour"),
-("Md003","Traditional Method","Strip Irrigation method","Used in raise platform"),
-("Md004","Modern Method","Sprinkler system","Used in uneven lands"),
-("Md005","Modern Method","Drip irrigation method","Used when there is scarcity of water");
-
+--
+-- Indexes for table `userreg`
+--
 ALTER TABLE `userreg`
   ADD PRIMARY KEY (`pid`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emptb`
+--
+ALTER TABLE `emptb`
+  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `userreg`
+--
 ALTER TABLE `userreg`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `medicines`
+--
+ALTER TABLE `medicines`
+  ADD CONSTRAINT `medicines_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `plant` (`plant_id`) ON DELETE CASCADE;
 COMMIT;
 
-create table manages(
-    eid integer not null,
-    ename varchar(20) not null,
-    plant_id varchar(20) not null,
-    plant_name varchar(20) not null,
-    med_id varchar(20) not null,
-    med_name varchar(20) not null,
-    salary integer not null
-);
-
-INSERT INTO manages VALUES
-(1,"ashok","P001","Tomato","M001","Tafaban",500),
-(2,"arun","P002","Sugarcane","M002","Fame",600),
-(3,"Dinesh","P003","Paddy","M003","Sectin",700),
-(4,"Ganesh","P004","Sunflower","M004","Atrataf",550),
-(5,"Kumar","P005","Cotton","M005","Tata Metri",800),
-(6,"Amit","P001","Tomato","M002","Fame",1000),
-(7,"Abbis","P002","Sugarcane","M003","Sectin",1500),
-(8,"Tiwary","P003","Paddy","M004","Atrataf",450)
-;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
